de829baba77a2617006c9d934482921026599489
Johnny Proano <jproano22@yahoo.com>
2024-09-20 14:37:22 -0400

    Initial Commit

---
+++ MealDelivery/MealDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealDeliveryApp.swift
+//  MealDelivery
+//
+//  Created by Johnny Proano on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MealDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
